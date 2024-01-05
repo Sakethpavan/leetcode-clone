8587411dee69c0f939d087d1b4c7d7b4c014822b
sakethpavan <sakethpavan21@gmail.com>
2024-01-06 05:28:31 +0530

    updated tailwind config

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -4,7 +4,9 @@ interface MainProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Main: React.FC<MainProps> = ({ children }) => {
   return (
     <div>
-      <aside></aside> // add something like side-bar
+      {/* add something like side bar here */}
+      <aside></aside> 
+      {/* render main contents here */}
       <main>
         {children}
       </main>
